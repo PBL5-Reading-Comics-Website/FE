a040fa32ed3b4c12cf4486b2802513140c6891c2
Nhan <nguyenngocbaonhan2003@gmail.com>
2024-05-10 08:35:26 +0700

    update

--- comic_reading/components/posting_page/newManga.tsx
+++ comic_reading/components/posting_page/newManga.tsx
@@ -1,15 +1,32 @@
 "use client";
-import { Input } from '../util/input.tsx';
+import {Input} from '../util/input.tsx';
 import Header from "../util/header";
 import ImgUpload from "./imgUpload.tsx";
 import TagListSelector from "./tagListSelector.tsx";
+import {useState} from "react";
 
 export function NewManga() {
+    const [manga, setManga] = useState({
+        name: '',
+        publishingCompany: '',
+        author: '',
+        artist: '',
+        description: '',
+        tags: [],
+        coverImage: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setManga({
+        ...manga,
+        [e.target.name]: e.target.value
+    });
+}
 
     return (
         <div>
             <div className="w-full h-full">
-                <Header />
+                <Header/>
             </div>
             <div className='top-20 mt-28 w-full p-5'>
                 <div className='flex w-full'>
@@ -21,32 +38,39 @@ export function NewManga() {
                                     <label className="block  text-sm font-bold mb-1" htmlFor="name">
                                         Tên truyện
                                     </label>
-                                    <Input id="name" type="text" className="h-10" />
+                                    <Input id="name" type="text" className="h-10" value={manga.name}
+                                           onChange={handleChange}/>
                                 </div>
                                 <div className="mb-2">
-                                    <label className="block  text-sm font-bold mb-1" htmlFor="name">
+                                    <label className="block  text-sm font-bold mb-1" htmlFor="publishing_company">
                                         Nhà xuất bản
                                     </label>
-                                    <Input id="name" type="text" className="h-10" />
+                                    <Input id="publishing_company" type="text" className="h-10"
+                                           value={manga.publishingCompany} onChange={handleChange}/>
                                 </div>
                                 <div className="mb-2">
-                                    <label className="block  text-sm font-bold mb-1" htmlFor="name">
+                                    <label className="block  text-sm font-bold mb-1" htmlFor="author">
                                         Tác giả
                                     </label>
-                                    <Input id="name" type="text" className="h-10" />
+                                    <Input id="author" type="text" className="h-10" value={manga.author}
+                                           onChange={handleChange}/>
                                 </div>
                                 <div className="mb-2">
-                                    <label className="block  text-sm font-bold mb-1" htmlFor="name">
+                                    <label className="block  text-sm font-bold mb-1" htmlFor="artist">
                                         Họa sĩ
                                     </label>
-                                    <Input id="name" type="text" className="h-10" />
+                                    <Input id="artist" type="text" className="h-10" value={manga.artist}
+                                           onChange={handleChange}/>
                                 </div>
                                 <div className="mb-2">
-                                    <label className="block  text-sm font-bold mb-1" htmlFor="email">
+                                    <label className="block  text-sm font-bold mb-1" htmlFor="description">
                                         Tóm tắt
                                     </label>
-                                    <textarea className='rounded-lg bg-zinc-800 border-2 border-neutral-500 resize-none focus:outline-none h-32 w-full' name="" id=""></textarea>
                                 </div>
+                                <textarea
+                                    className='rounded-lg bg-zinc-800 border-2 border-neutral-500 resize-none focus:outline-none h-32 w-full'
+                                    id="description" value={manga.description} onChange={handleChange}>
+                                </textarea>
 
                             </div>
                         </div>
@@ -55,14 +79,14 @@ export function NewManga() {
                         <label className='block  text-sm font-bold mb-1' htmlFor="name">
                             Bìa truyện
                         </label>
-                        <ImgUpload />
+                        <ImgUpload/>
                     </div>
                 </div>
                 <div className="mb-2 z-10 w-full border-b pb-4">
                     <label className="block  text-sm font-bold mb-1" htmlFor="email">
                         Chọn thể loại
                     </label>
-                    <TagListSelector />
+                    <TagListSelector/>
                 </div>
             </div>
         </div>
